9c1c3c84106233464a6b79d1643b1610301d4143
Kamile Silva <92469257+KamileSilva@users.noreply.github.com>
2021-10-14 17:30:23 -0300

    Add files via upload

---
+++ SPBrasil/boleto.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<title> SP Brasil </title>
+
+<meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+
+<!--------------------------------------------------------------------- Imagem Barra ------------------------------------------------------------------------->
+
+<link rel="shortcut icon" href="./assets/images/logo.png">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/estilo.css">
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+</head>
+
+<body>
+
+<!------------------------------------------------------------------------- Topo ----------------------------------------------------------------------------->
+
+<nav class="navbar navbar-expand-md justify-content-center">
+<img src="assets/images/logotopo.png" width="200px" height="75px">
+</nav>
+
+
+<!------------------------------------------------------------------------Corpo/Cadastro -------------------------------------------------------------->
+<div class="container">
+	<div class="panel cor">
+  <div class="panel-body"><h2 class="display-5" align="center"> Área do Cliente </h2></div>
+</div>
+	
+	<div class="row">
+    <div class="col-md-3">
+		<p>
+		 <ul>
+		  <li> <form action="https://taiga.hinova.com.br/sga/area/4.1/login.action.php" method="post" >  
+<b>CPF</b>
+<br>
+ <input type="text" name="dfsCpf" id="dfsCpf" > 
+ </li>
+
+ <li>
+ <b>Senha </b>
+ <input type="password" name="dfsSenha" id="dfsSenha" > 
+ <br>
+ <input type="hidden" name="dfsChave" id="dfsChave" value="c09be3cd2ff8df444e8fa80b81fea4be249f869ee302c31520c6cd0263fc1b1f2c480b257cee32d435c36ba01fcc378e08c93da1bd87fcb509b8e7a019b128be" >
+ <br>
+<input type="submit" name="pbEntrar" value="Entrar"> 
+<br>
+<a href="https://taiga.hinova.com.br/sga/area/4.1/lembrarSenha.php?chave=ce32fde8bd87a673d6930f45b6702dd3d5838de2a63345014858c446e59a33dacb120308f9a3f0f31399027edd7613d2ce4473a6f9384c37b5d82ecb3bb8deaa" target="_blank" ><br><b> <u> Lembrar Senha</u></b> </a>  
+</form>
+
+ </li>
+  
+
+		 </ul>
+		</p>
+		</div>
+
+</body>
+</html>
---
+++ SPBrasil/index.php
@@ -0,0 +1,365 @@
+<html>
+<head>
+<title> SP Brasil </title>
+
+<meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+
+<!--------------------------------------------------------------------- Imagem Barra ------------------------------------------------------------------------->
+
+<link rel="shortcut icon" href="./assets/images/logo.png">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/estilo.css">
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+</head>
+
+<body>
+
+<!------------------------------------------------------------------------- Topo ----------------------------------------------------------------------------->
+<div class="container top">
+	<div class="row">
+		<div class="col-md-3">
+		</div>
+		<div class="col-md-6" align="center">
+			<img src="assets/images/logotopo.png" width="200px" height="70px">
+		</div>
+		<div class="col-md-3" align="center">
+			<p> 
+			<a href="#" data-toggle="modal" data-target="#exampleModal"> 
+			<img src="assets/images/login.png" width="50px" height="50px"> <br>
+			Área do Cliente </a>
+			
+			<div class="modal fade" id="exampleModal" tabindex="1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<div class="modal-header">
+			<h5 class="modal-title" id="exampleModalLabel"> Área do Cliente </h5>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true"> &times; </span>
+			</button>
+			</div>
+		<div class="modal-body"> 
+		<p>
+		<form action="https://taiga.hinova.com.br/sga/area/4.1/login.action.php" method="post" >  
+		<b>CPF</b>
+		<input type="text" name="dfsCpf" id="dfsCpf" > 
+		<br><br>
+		<b>Senha </b>
+ <input type="password" name="dfsSenha" id="dfsSenha" > 
+ <br>
+ <input type="hidden" name="dfsChave" id="dfsChave" value="c09be3cd2ff8df444e8fa80b81fea4be249f869ee302c31520c6cd0263fc1b1f2c480b257cee32d435c36ba01fcc378e08c93da1bd87fcb509b8e7a019b128be" >
+ <br>
+<input type="submit" name="pbEntrar" value="Entrar"> 
+<br>
+<a href="https://taiga.hinova.com.br/sga/area/4.1/lembrarSenha.php?chave=ce32fde8bd87a673d6930f45b6702dd3d5838de2a63345014858c446e59a33dacb120308f9a3f0f31399027edd7613d2ce4473a6f9384c37b5d82ecb3bb8deaa" target="_blank" ><br><b> <u> Lembrar Senha</u></b> </a>  
+</form>
+
+		</p>
+		</div>
+		<div class="modal-footer">
+		<button type="button" class="btn btn-secondary" data-dismiss="modal"> Fechar </button>
+		</div>
+		</div>
+	</div>
+</div>
+</p>
+
+		</div>
+	</div>
+</div>
+
+<!---------------------------------------------------------------------- Carousel ---------------------------------------------------------------------------->
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="assets/images/slide_2.png" width="100%" alt="Chicago">
+		</div>
+		<div class="carousel-item">
+			<img src="assets/images/slide_3.png" width="100%" alt="Chicago">
+		</div>
+		<div class="carousel-item">
+			<img src="assets/images/slide_4.png" width="100%" alt="Chicago">
+		</div>
+	</div>
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+<span class="carousel-control-prev-icon"> <span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+<span class="carousel-control-next-icon"> </span>
+</a>
+</div>
+<br>
+
+<main>
+
+<!--------------------------------------------------------------- Informações Empresa ------------------------------------------------------------------------>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+	<div class="container">
+		<h2> &nbspSobre Nós </h2>
+		<div class="row">
+			<div class="col-md-9">
+				<p>
+					Com o objetivo de trazer mudança e qualidade ao segmento dentro do Estado de São Paulo,
+					a SP BRASIL Proteção iniciou suas atividades com melhor time de gestores e
+					coordenadores do ramo de proteção veicular.<br><br>
+					Com sede em São Paulo capital e filiais em São Bernardo do campo, Mauá, Rio de Janeiro,
+					Bahia e Interior de SP a SP BRASIL é uma associação sem fins lucrativos com objetivo de
+					proporcionar a seus associados, benefícios que agregam vários serviços.<br><br>
+					Investindo constantemente em qualidade e estrutura, nossa meta é oferecer a segurança e
+					tranquilidade ao associado para utilizar o seu patrimônio sem maiores preocupações.<br><br>
+					Continuamos em busca dos melhores parceiros e serviços para atingir a maior satisfação
+					possível por parte de todos os que se associam a SP BRASIL Proteção Veicular.
+				</p>
+			</div>
+			<div class="col-md-3" align="center">
+				<img src="./assets/images/img.png" class="img-thumbnail" width="300px" height="300px">
+			</div>
+		</div>
+	</div>
+  </div>
+</div>
+<br>
+
+
+<!------------------------------------------------------------- Missao, visao, valores ----------------------------------------------------------------------->
+
+<div class="container">
+<div class="card-deck" align="center">
+  <div class="card">
+    <div class="card-body">
+      <h3> Missão </h3>
+			<p>
+			Proporcionar aos nossos associados, segurança, tranquilidade e comodidade.
+			</p>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h3> Visão </h3>
+			<p>
+			Fidelizar nossos associados através da transparência e ética. Manter parcerias com os
+			melhores prestadores de serviços.
+			</p>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h3> Valores </h3>
+			<p>
+			Ética, compromisso, eficiência, transparência.
+			</p>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<!------------------------------------------------------------- Política de Qualidade ------------------------------------------------------------------------>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+	<div class="container">
+		<h2> &nbspPolítica de Qualidade </h2>
+		<p>
+			<ul>
+				<li> Buscar a satisfação dos seus associados e terceiros, através de serviços que
+					atendam os seus requisitos de qualidade. </li>
+				<li> Buscar a satisfação e oferecer condições de trabalho aos colaboradores, Gestores,
+					Coordenadores e consultores comerciais;</li>
+				<li> Avaliar os fornecedores na busca da qualidade dos serviços oferecidos; </li>
+				<li> Atender aos requisitos legais aplicáveis e outros subscritos pela Associação e AAAPV
+					– Agência de Auto-regulamentação das Entidades de Autogestão de Planos de
+					Proteção Contra Riscos Patrimoniais; </li>
+				<li> Promover a melhoria contínua do sistema de gestão da qualidade, através do
+					monitoramento de seus objetivos e metas.</li>
+			</ul>
+		</p>
+		
+	</div>
+  </div>
+</div>
+<br>
+<hr>
+
+<!-------------------------------------------------------------- Nossas Coberturas -------------------------------------------------------------------------->
+
+<div class="container">
+	<h2 class="display-5" align="center"> Nossas Coberturas </h2>
+	<div class="row">
+		<div class="col-md-3">
+		<p>
+		 <ul>
+		  <li> Roubo e furto </li>
+		 </ul>	
+		</p>
+		</div>
+		<div class="col-md-3">
+		<p>
+		 <ul>
+		  <li> Colisão e perda total </li>
+		 </ul>	
+		</p>
+		</div>
+		<div class="col-md-3">
+		<p>
+		 <ul>
+		  <li> Incêndio e Fenômenos da natureza </li>
+		 </ul>	
+		</p>
+		</div>
+		<div class="col-md-3">
+		<p>
+		 <ul>
+		  <li> Danos materiais a terceiros de até R$100.000,00 </li>
+		 </ul>	
+		</p>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-3">
+		<p>
+
+		</div>
+		<div class="col-md-6">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			  <ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+			  </ol>
+			  <div class="carousel-inner">
+				<div class="carousel-item active">
+				  <img src="./assets/images/prot1.png" class="img-thumbnail" alt="First slide" width="100%">
+				</div>
+				<div class="carousel-item">
+				  <img src="./assets/images/prot2.png" class="img-thumbnail" alt="Second slide" width="100%">
+				</div>
+				<div class="carousel-item">
+				  <img src="./assets/images/prot3.png" class="img-thumbnail" alt="Third slide" width="100%">
+				</div>
+				<div class="carousel-item">
+				  <img src="./assets/images/prot4.png" class="img-thumbnail" alt="Third slide" width="100%">
+				</div>
+			  </div>
+			  
+			  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			  </a>
+			</div>
+		</div>
+		<div class="col-md-3">
+		
+		</div>
+	</div>
+	
+</div>
+<br>
+
+<!------------------------------------------------------------------- Assistência 24h ------------------------------------------------------------------------>
+
+<div class="container">
+	<div class="jumbotron" align="center">
+		<h2 class="display-5" align="center"> Assistência 24hrs </h2>
+		<div class="row">
+		<div class="col-md-4">
+		<p>
+			<ul align="left">
+				<li> Reboque de até 1000km </li>
+				<li> Borracheiro </li>
+				<li> Chaveiro </li>
+			</ul>
+		</p>
+		</div>
+		<div class="col-md-5">
+		<p>
+			<ul align="left">
+				<li> Carro reserva de até 30 dias </li>
+				<li> Rastreamento e monitoramento </li>
+				<li> Atendimento em todo território nacional </li>
+			</ul>
+		</p>
+		</div>
+		<div class="col-md-3">
+		<p>
+			<ul align="left">
+				<li> Pane seca </li>
+				<li> Pane elétrica </li>
+				<li> Pane mecânica </li>
+			</ul>
+		</p>
+		</div>
+		</div>
+	<br>
+	<i><h6 class="display-5" align="center"> Sem análise de perfil de condutor, sem consulta sps/serasa, sem análise de cep,
+	cobertura 100% tabela FIPE.</h6></i>
+	<br>
+	<a href="https://api.whatsapp.com/send?phone=5511966692525" target="_blank" class="btn" style="width:50%" role="button"> Obter Proteção&raquo;</a>
+	</div>
+</div>
+
+</main>
+
+<!--------------------------------------------------------------------- Rodapé ------------------------------------------------------------------------------->
+
+<hr>
+<footer class="container">
+
+	<div class="row">
+		<div class="col-md-3" align="center">
+		<p> <b> Central de Monitoramento: </b> <br> 0800-760-0500 </p>
+		</div>
+		<div class="col-md-3" align="center">
+		<p> <b> Assistência 24Hrs: </b> <br> 0800-649-0300 </p>
+		</div>
+		<div class="col-md-3" align="center">
+		<p> <b>Whatsapp:</b>
+		 <a href="https://api.whatsapp.com/send?phone=5511966692525" target="_blank" ><br>(11)966692525</a> </p>
+		</div>
+		<div class="col-md-3" align="center">
+		<p> <b> E-mail </b> <br> 
+		suporte@spbrasilprotecao.com.br 
+		</p>
+		</div>
+		
+	</div>
+	
+	<div class="row">
+		<div class="col-md-4" align="center"> <br>
+		<p> &copy; Developer Inc. 2021 </p>
+		</div>
+		<div class="col-md-4" align="center">
+		<p> <b> Webdesigner </b> <br>
+		Kamile da Silva e Silva <br>
+		Rômulo César Silva Brito
+		</p>
+		</div>
+		<div class="col-md-4" align="center">
+		<p> <b> Escritório Comercial </b> <br>
+		Alameda Dona Tereza Cristina, 554 <br>
+		Nova Petrópolis | São Bernardo do Campo - SP
+		</p>
+		</div>
+		
+	</div>
+	
+	
+</footer>
+
+<!--chama o jquery-->
+<script type="text/javascript" src="assets/js/jquery-3.5.0.min.js"> </script>
+<!--chama o bundle-->
+<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"> </script>
+
+</body>
+</html>
